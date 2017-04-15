c142684f8442f1b83d0a800533aefea1bd9b4192
kunisue <kunitatu777@gmail.com>
2017-04-15 22:37:45 +0900

    mod

--- src/test/java/com/kunitatu/kuniselenium/KuniSeleniumGoogleTestCase.java
+++ src/test/java/com/kunitatu/kuniselenium/KuniSeleniumGoogleTestCase.java
@@ -1,22 +1,7 @@
 package com.kunitatu.kuniselenium;
 
-import java.io.File;
-
-import org.apache.commons.io.FileUtils;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.ie.InternetExplorerDriverService;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.Wait;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import junit.framework.TestCase;
 
 /**
  * Unit test for simple App.
